2b1fcafe693e462da5d2af6aeed6f220732d4a52
atina <astanisz1@gmail.com>
2015-11-16 18:03:02 +0100

    Crossover implementation.

--- src/main/java/ipd/evolution/impl/CrossoverImpl.java
+++ src/main/java/ipd/evolution/impl/CrossoverImpl.java
@@ -2,13 +2,20 @@ package ipd.evolution.impl;
 
 import ipd.evolution.Crossover;
 import ipd.model.game.Player;
+import ipd.utils.ProbabilityUtils;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 public class CrossoverImpl implements Crossover {
+	private static final double copyProbability = 0.3;
 
 	public void cross(List<Player> players) {
-		// TODO:
+		List<Player> parentPopulation = Lists.newLinkedList(players);
+		for (int i = 1; i < parentPopulation.size(); i += 2) {
+			if (ProbabilityUtils.simulateProbability(copyProbability))
+				players.set(i, parentPopulation.get(i - 1));
+		}
 	}
-
 }
